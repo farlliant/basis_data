1e5fd0c6fdbb20dcf77d7441c702963d10cfedd8
farrel <farrellbrilliantt@gmail.com>
2025-06-06 16:48:01 +0700

    api documentation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,12 @@
-# backend/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-# --- Import yang diperlukan untuk dokumentasi ---
+# --- Import untuk dokumentasi ---
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-# ----------------------------------------------
 
-# --- Konfigurasi untuk halaman dokumentasi ---
 schema_view = get_schema_view(
    openapi.Info(
       title="API Laris Amanah",
@@ -22,19 +18,13 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-# --------------------------------------------
 
 urlpatterns = [
-    # URL Asli Kamu
     path('admin/', admin.site.urls),
     path('api/', include('produk.urls')),
     path('user/', include('user.urls')),
     path('', lambda request: HttpResponse("Welcome to the API!")),
-
-    # === URL UNTUK DOKUMENTASI API ===
-    # Ini akan membuat halaman Swagger UI yang interaktif
+    # URL untuk dokumentasi API
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    
-    # (Opsional) Ini membuat halaman dokumentasi alternatif (ReDoc)
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
